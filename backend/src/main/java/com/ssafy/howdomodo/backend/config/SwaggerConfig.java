b8ff877275ebe7f6313eb996eb944f84d1661599
전수현 <soohyun0907@naver.com>
2020-09-16 15:33:40 +0900

    Update Backend Code

--- backend/src/main/java/com/ssafy/howdomodo/backend/config/SwaggerConfig.java
+++ backend/src/main/java/com/ssafy/howdomodo/backend/config/SwaggerConfig.java
@@ -1,10 +1,8 @@
 package com.ssafy.howdomodo.backend.config;
 
 import com.google.common.base.Predicates;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -14,11 +12,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    
     @Bean
-    public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2).select()
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
                 .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
-                .paths(PathSelectors.any()).build();
+                // .apis(RequestHandlerSelectors.basePackage("com.ssafy.howdomodo.backend.controller"))
+                .paths(PathSelectors.ant("/api/**"))
+                .build();
     }
 }
