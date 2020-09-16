2026bc7e70843988ffdb0b12e3b28a53c9e4d98b
KCY <ghkdua059@gmail.com>
2020-09-16 15:25:41 +0900

    Add 뷰모델 초기 코드 세팅 | S03P22A305-126

--- frontend/HowDoMoDo/app/src/main/java/com/ssafy/howdomodo/ui/signup/SignUpViewModel.kt
+++ frontend/HowDoMoDo/app/src/main/java/com/ssafy/howdomodo/ui/signup/SignUpViewModel.kt
@@ -1,8 +1,21 @@
 package com.ssafy.howdomodo.ui.signup
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.gson.JsonObject
 import com.ssafy.howdomodo.data.repository.SignUpRepository
 
 class SignUpViewModel(private val signUpRepository: SignUpRepository):ViewModel(){
+    val token = MutableLiveData<String>()
+    val signUpError = MutableLiveData<String>()
+
+
+    fun signUp(userName: JsonObject) {
+        signUpRepository.signUp(userName, success = {
+            token.value = it
+        }, fail = {
+            signUpError.value = it.message
+        })
+
+    }
 }
