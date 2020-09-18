bce60cd6b82b6ed9542cb75b467a88b3b7eb1783
KCY <ghkdua059@gmail.com>
2020-09-18 18:12:09 +0900

    Feat 영화관 선택 추가

---
+++ frontend/HowDoMoDo/app/src/main/java/com/ssafy/howdomodo/ui/gwanSelect/GwanSelectActivity.kt
@@ -0,0 +1,13 @@
+package com.ssafy.howdomodo.ui.gwanSelect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ssafy.howdomodo.R
+
+class GwanSelectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gwan_select)
+
+    }
+}
