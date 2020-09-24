5d592696e24bda3791a00525989bfaeebcba2f87
KCY <ghkdua059@gmail.com>
2020-09-24 15:44:39 +0900

    Feat 마이페이지 구성 | S03P23A305-96

--- frontend/HowDoMoDo/app/src/main/java/com/ssafy/howdomodo/ui/bottomtap/BottomTabAdapter.kt
+++ frontend/HowDoMoDo/app/src/main/java/com/ssafy/howdomodo/ui/bottomtap/BottomTabAdapter.kt
@@ -3,6 +3,7 @@ package com.ssafy.howdomodo.ui.bottomtap
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import com.ssafy.howdomodo.ui.Favorite.FavoriteFragment
 import com.ssafy.howdomodo.ui.main.MainFragment
 import com.ssafy.howdomodo.ui.mypage.MypageFragment
 
@@ -11,8 +12,8 @@ class BottomTabAdapter(fm: FragmentManager, val fragmentCount: Int) :
 
     override fun getItem(position: Int): Fragment {
         when (position) {
-            0 -> return MypageFragment()
-            1 -> return MainFragment()
+            0 -> return MainFragment()
+            1 -> return FavoriteFragment()
             2 -> return MypageFragment()
             3 -> return MypageFragment()
             else -> null!!
