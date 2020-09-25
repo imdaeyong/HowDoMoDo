52a82eb9e2941776dc77683ae6fe0fb6f2e151c0
전수현 <soohyun0907@naver.com>
2020-09-25 12:23:21 +0900

    Feat send theaters info by Soo-Hyun | S03P23A305-24, S03P23A305-44

--- backend/src/main/java/com/ssafy/howdomodo/controller/TheaterController.java
+++ backend/src/main/java/com/ssafy/howdomodo/controller/TheaterController.java
@@ -7,10 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.howdomodo.domain.Cities;
+import com.ssafy.howdomodo.domain.Theaters;
 import com.ssafy.howdomodo.model.Response;
 import com.ssafy.howdomodo.model.ResponseMessage;
 import com.ssafy.howdomodo.model.RestException;
@@ -37,5 +39,17 @@ public class TheaterController {
 			throw new RestException(ResponseMessage.SEARCH_SIDO_INFO_FAIL, HttpStatus.FORBIDDEN);
 		
 		return new ResponseEntity<Response>(new Response(StatusCode.OK, ResponseMessage.SEARCH_SIDO_INFO_SUCCESS, cities), HttpStatus.OK);
+	}
+	
+	@ApiOperation(value="시도 정보에 따른 영화관을 반환합니다.")
+	@GetMapping("/{siName}/{guName}")
+	public ResponseEntity theatersInfo(@PathVariable("siName") String siName, @PathVariable("guName") String guName) {
+		System.out.println(siName+ " / " + guName);
+		int cityId = theaterService.getCityId(siName, guName);
+		List<Theaters> theaters = theaterService.getTheatersInfo(cityId);
+		if(theaters == null)
+			throw new RestException(ResponseMessage.SEARCH_THEATERS_INfO_NONE, HttpStatus.FORBIDDEN);
+		
+		return new ResponseEntity<Response>(new Response(StatusCode.OK, ResponseMessage.SEARCH_THEATERS_INFO_SUCCESS, theaters), HttpStatus.OK);
 	}
 }
--- backend/src/main/java/com/ssafy/howdomodo/service/TheaterServiceImpl.java
+++ backend/src/main/java/com/ssafy/howdomodo/service/TheaterServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.howdomodo.domain.Cities;
+import com.ssafy.howdomodo.domain.Theaters;
 import com.ssafy.howdomodo.mapper.TheatersMapper;
 
 @Service
@@ -21,6 +22,28 @@ public class TheaterServiceImpl implements TheaterService {
 		try {
 			cities = theaterMapper.getAllSido();
 			return cities;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	@Override
+	public int getCityId(String siName, String guName) {
+		try {
+			int res = theaterMapper.getCityId(siName, guName);
+			return res;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return -1;
+		}
+	}
+
+	@Override
+	public List<Theaters> getTheatersInfo(int cityId) {
+		try {
+			List<Theaters> theaters = theaterMapper.getTheaterInfo(cityId);
+			return theaters;
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return null;
