bf505d9837a18b3bf85f9d8b375ccfa2f671d9c7
이선수 <tjstndi77@naver.com>
2020-10-01 19:22:39 +0900

    Modify url | S03P23A305-102

--- backend_api/bigdata/urls.py
+++ backend_api/bigdata/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.create_load),
-    path('result/', views.preProcess),
-    path('find_si/<str:siname>', views.find_si),
+    # path('', views.create_load),
+    # path('result/', views.preProcess),
+    # path('find_si/<str:siname>', views.find_si),
 ]
