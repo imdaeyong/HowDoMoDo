2ed4e366fcab7830894a10380a35ae5fcf563989
linkcoder-py <rnjs213@gmail.com>
2020-10-01 11:05:22 +0900

    Update django server deploy by Ohjeong | S03P23A305-103

--- backend_api/api/urls.py
+++ backend_api/api/urls.py
@@ -5,12 +5,6 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('a', views.tester),
-    path('timestables/<brand>/<name>/<stime>', views.get_times_tables),
-
-=======
     path('suns/<title>', views.suns),
     path('timestables/<brand>/<name>/<date>/<title>', views.get_times_tables),
->>>>>>> 49363d3db6adf6de684925cc2a6ed574ae1fa6d4
 ]
